4f8927eecbffe5df04b46701a654539c303f8481
alexant82 <99618082+alexant82@users.noreply.github.com>
2022-02-14 20:45:46 +0300

    Задача 6

---
+++ Циклы Задача 6.cpp
@@ -0,0 +1,39 @@
+﻿// Задание 6. Вывести на экран все числа от нуля до введенного пользователем числа.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int a, b;
+
+    cout << endl;
+    cout << endl;
+    cout << "\t\tВведите число, до которого программа выведет все числа от нуля"<<"  ";
+    cin >> b;
+   
+    cout << "\t\t_____________________________________________________________";
+    cout << endl;
+    cout << endl;
+    
+
+    for (int i = 0; i <= b ; i++) {
+        cout <<"\t\t\t\t\t"<< "   " << i;
+        cout << endl;
+
+    }
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    return 0;
+    
+}
